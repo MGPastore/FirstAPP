f70ca69dde118b0a03bd310a2a004065bc0ab6e9
MGPastore <137975449+MGPastore@users.noreply.github.com>
2023-09-07 23:42:35 +0000

    more_routes

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const sequelize = new Sequelize('test', 'root', '', {
 const app = express()
 const port = 3000
 
-var cors = require('cors') 
+var cors = require('cors')
 app.use(cors())
 app.use(express.json());
 var sanitize = require("./middleware/sanitize")
@@ -28,145 +28,153 @@ app.use(sanitize)
 
 // Modelos Sequelize
 const Form = sequelize.define('Form', {
-    id: {
-      type: Sequelize.STRING(36),
-      primaryKey: true,
+  id: {
+    type: Sequelize.STRING(36),
+    primaryKey: true,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+    onUpdate: Sequelize.NOW,
+  },
+});
+
+const Question = sequelize.define('Question', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    unique: true,
+  },
+  userId: {
+    type: Sequelize.STRING(36),
+    allowNull: false,
+  },
+  text: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+    onUpdate: Sequelize.NOW,
+  },
+});
+
+const Response = sequelize.define('Response', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  formId: {
+    type: Sequelize.STRING(36),
+    allowNull: false,
+    references: {
+      model: Form,
+      key: 'id',
     },
-    createdAt: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW,
+  },
+  questionId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: Question,
+      key: 'id',
     },
-    updatedAt: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW,
-      onUpdate: Sequelize.NOW,
-    },
+  },
+  answer: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+    onUpdate: Sequelize.NOW,
+  },
+});
+
+// Sincronizar modelos con la base de datos
+sequelize.sync()
+  .then(() => {
+    console.log('Tablas sincronizadas con la base de datos.');
+  })
+  .catch(error => {
+    console.error('Error al sincronizar tablas:', error);
   });
-  
-  const Question = sequelize.define('Question', {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      unique: true,
-    },
-    userId: {
-        type: Sequelize.STRING(36),
-        allowNull: false,
-      },
-    text: {
-      type: Sequelize.TEXT,
-      allowNull: false,
-    },
-    createdAt: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW,
-    },
-    updatedAt: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW,
-      onUpdate: Sequelize.NOW,
-    },
-  });
-  
-  const Response = sequelize.define('Response', {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    formId: {
-      type: Sequelize.STRING(36),
-      allowNull: false,
-      references: {
-        model: Form,
-        key: 'id',
-      },
-    },
-    questionId: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      references: {
-        model: Question,
-        key: 'id',
-      },
-    },
-    answer: {
-      type: Sequelize.TEXT,
-      allowNull: false,
-    },
-    createdAt: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW,
-    },
-    updatedAt: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW,
-      onUpdate: Sequelize.NOW,
-    },
-  });
-  
-  // Sincronizar modelos con la base de datos
-  sequelize.sync()
-    .then(() => {
-      console.log('Tablas sincronizadas con la base de datos.');
-    })
-    .catch(error => {
-      console.error('Error al sincronizar tablas:', error);
-    });
-  
+
 
 app.get('/GET/Question/:ID', async (req, res) => {
-    var data =  await Question.findAll({
-        where: {
-            userId : req.params.ID
-        }
-      })
-      console.log(data)
-      res.json(data)
-})
-// SE TIENE QUE ARREGLAR PARA PASARLE EL ID PR PARAMETROS
-app.post("/POST/:IDFORM/RESPONSE",async(req,res)=>{
-
-try {
-  var {IDFORM}=req.params
-  var {questionId,answer} = req.body
-  console.log(IDFORM+ " "+questionId,answer)
-    //await Form.create({id: 1})
-   //await Response.create({formId:1,questionId:1,answer:"Gabriel"})
-   await Response.bulkCreate(req.body)
-res.json("POST API WORK")
-
-} catch (error) {
-  res.json(error)
-}
-
-
-})
-
-
-  app.post('/POST/QUESTION', async (req, res) => {
-    try {
-     /* const data = [
-        { text: '¿Cómo te llamas?',
-        userId: 'MGP' },
-        { text: '¿Cuál es tu edad?',
-        userId: 'MGP' },
-        { text: '¿Cuál es tu ciudad natal?',
-        userId: 'MGP' },
-      ];
-        */
-       var data = req.body
-      await Question.bulkCreate(data);
-  
-      res.json('Preguntas agregadas correctamente :)')
-    } catch (error) {
-      res.json('Error al crear preguntas');
+  var data = await Question.findAll({
+    where: {
+      userId: req.params.ID
     }
-  });
+  })
+  console.log(data)
+  res.json(data)
+})
+
+app.post("/POST/:IDFORM/Response", async (req, res) => {
+
+  try {
+    //EL ID TIENE QUE SER CREADO POR UN UUID
+    await Form.create({id: 1})
+    res.json(json = "New FormID")
+
+  } catch (error) {
+    res.json(error)
+  }
 
 
-  app.use('/public', express.static(__dirname + '/public'));
+})
+app.post("/POST/:IDFORM/Response", async (req, res) => {
+
+  try {
+    //await Form.create({id: 1})
+    //await Response.create({formId:1,questionId:1,answer:"Gabriel"})
+    await Response.bulkCreate(req.body)
+    res.json(json = "postDataOK")
+
+  } catch (error) {
+    res.json(error)
+  }
+
+
+})
+
+
+app.get('/POST/Question', async (req, res) => {
+  try {
+     const data = [
+       { text: 'Usuario',
+       userId: 'MGPB' },
+       { text: 'Contraseña',
+       userId: 'MGPB' }
+     ];
+      
+    //var data = req.body
+    await Question.bulkCreate(data);
+
+    res.json(json = "newQuestionOK")
+  } catch (error) {
+    res.json('Error al crear preguntas');
+  }
+});
+
+
+app.use('/public', express.static(__dirname + '/public'));
 
 app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`)
@@ -179,4 +187,4 @@ app.listen(port, async () => {
 })
 
 
-module.exports = {Response, Question, Form  }
+module.exports = { Response, Question, Form }
