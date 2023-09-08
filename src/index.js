2d0906ca78d330d8b5e1c85be7652472f515ce49
MGPastore <137975449+MGPastore@users.noreply.github.com>
2023-09-07 22:18:58 -0300

    check

--- src/index.js
+++ src/index.js
@@ -128,7 +128,7 @@ sequelize.sync()
 app.get('/GET/Question/:ID', async (req, res) => {
   var data = await Question.findAll({
     where: {
-      userId: req.params.ID
+      userId: parseInt(req.params.ID)
     }
   })
   console.log(data)
